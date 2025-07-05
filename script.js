062d6e462e08edce95ae20d79a8cd1c4bfb7f890
jishhh7 <jishikatudumu8@gmail.com>
2025-07-06 03:56:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function openEnvelope() {
   const envelope = document.querySelector('.envelope');
+  document.getElementById('flap').style.transform = 'rotateX(-180deg)';
+  document.getElementById('letter').style.transform = 'translateY(0)';
+  document.getElementById('bgImage').style.opacity = 1;
+  document.getElementById('bgm').play();
+
   if (!envelope.classList.contains('open')) {
     envelope.classList.add('open');
     document.body.classList.add('opened'); // Makes background visible
