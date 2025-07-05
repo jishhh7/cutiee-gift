18a51d9a93ca8120797cc607aaaf15d44353d229
jishhh7 <jishikatudumu8@gmail.com>
2025-07-06 03:57:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 function openEnvelope() {
-  const envelope = document.querySelector('.envelope');
   document.getElementById('flap').style.transform = 'rotateX(-180deg)';
   document.getElementById('letter').style.transform = 'translateY(0)';
   document.getElementById('bgImage').style.opacity = 1;
   document.getElementById('bgm').play();
-
-  if (!envelope.classList.contains('open')) {
-    envelope.classList.add('open');
-    document.body.classList.add('opened'); // Makes background visible
-
-    const music = document.getElementById('bg-music');
-    if (music) {
-      music.play().catch(() => {
-        console.log("Autoplay blocked.");
-      });
-    }
-  }
 }
