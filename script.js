f97ae77381cd2b0adb440fa3e21c7374a768cd36
jishhh7 <jishikatudumu8@gmail.com>
2025-07-06 03:22:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function openEnvelope() {
   const envelope = document.querySelector('.envelope');
   if (!envelope.classList.contains('open')) {
     envelope.classList.add('open');
-    
+    document.body.classList.add('opened');
+
     const music = document.getElementById('bg-music');
-    if (music) {
-      music.play().catch(e => console.log("Autoplay blocked:", e));
-    }
+    if (music) music.play().catch(() => {});
   }
 }
